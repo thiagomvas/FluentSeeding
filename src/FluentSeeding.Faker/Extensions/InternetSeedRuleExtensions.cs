277a971151cdbb228e78e6d3af30ef6eec89adf7
Thiago Vasconcelos <thiagomvas@gmail.com>
2026-03-27 19:04:42 -0300

    feat: Add ipv4, ipv6 and mac address generators

--- src/FluentSeeding.Faker/Extensions/InternetSeedRuleExtensions.cs
+++ src/FluentSeeding.Faker/Extensions/InternetSeedRuleExtensions.cs
@@ -1,5 +1,6 @@
 using System.Globalization;
 using System.Linq.Expressions;
+using System.Runtime.InteropServices;
 using System.Text;
 using FluentSeeding.Faker.Locales;
 
@@ -59,6 +60,92 @@ public static class InternetSeedRuleExtensions
 
         var wrapped = Expression.Lambda<Func<T, string>>(concat, prefix.Parameters[0]);
         return rule.UseEntityFactory(wrapped);
+    }
+    
+    /// <summary>
+    /// Generates a random IPv4 address.
+    /// </summary>
+    /// <param name="rule">The seed rule to apply the factory to.</param>
+    /// <param name="formatted">
+    /// When <c>true</c>, returns a dot-notation address (e.g. <c>192.168.1.1</c>).
+    /// When <c>false</c>, returns the raw concatenated octets (e.g. <c>192168011</c>).
+    /// </param>
+    public static SeedBuilder<T> UseIpv4<T>(this SeedRule<T, string> rule, bool formatted = false) where T : class
+    {
+        return rule.UseFactory(() =>
+        {
+            Span<byte> bytes = stackalloc byte[4];
+            Random.Shared.NextBytes(bytes);
+
+            if (formatted)
+                return $"{bytes[0]}.{bytes[1]}.{bytes[2]}.{bytes[3]}";
+            return $"{bytes[0]}{bytes[1]}{bytes[2]}{bytes[3]}";
+        });
+    }
+    /// <summary>
+    /// Generates a random IPv6 address.
+    /// </summary>
+    /// <param name="rule">The seed rule to apply the factory to.</param>
+    /// <param name="formatted">
+    /// When <c>true</c>, returns a colon-separated groups address (e.g. <c>2001:0db8:85a3:0000:0000:8a2e:0370:7334</c>).
+    /// When <c>false</c>, returns the raw lowercase hex string (e.g. <c>20010db885a3000000008a2e03707334</c>).
+    /// </param>
+    public static SeedBuilder<T> UseIpv6<T>(this SeedRule<T, string> rule, bool formatted = false) where T : class
+    {
+        return rule.UseFactory(() =>
+        {
+            Span<byte> bytes = stackalloc byte[16];
+            Random.Shared.NextBytes(bytes);
+
+            if (!formatted)
+                return Convert.ToHexString(bytes).ToLowerInvariant();
+
+            Span<char> chars = stackalloc char[39];
+            var pos = 0;
+            for (var i = 0; i < 16; i += 2)
+            {
+                if (i > 0) chars[pos++] = ':';
+                var group = (ushort)((bytes[i] << 8) | bytes[i + 1]);
+                group.TryFormat(chars[pos..], out var written, "x4");
+                pos += written;
+            }
+            return new string(chars[..pos]);
+        });
+    }
+    
+    /// <summary>
+    /// Generates a random MAC address.
+    /// </summary>
+    /// <param name="rule">The seed rule to apply the factory to.</param>
+    /// <param name="separator">
+    /// The character used to separate each octet. Defaults to <c>:</c> (e.g. <c>00:1A:2B:3C:4D:5E</c>).
+    /// Use <c>'-'</c> for Windows-style (e.g. <c>00-1A-2B-3C-4D-5E</c>), or <c>null</c> for no separator
+    /// (e.g. <c>001A2B3C4D5E</c>).
+    /// </param>
+    /// <param name="uppercase">When <c>true</c>, returns uppercase hex digits. Defaults to <c>true</c>.</param>
+    public static SeedBuilder<T> UseMacAddress<T>(this SeedRule<T, string> rule, char? separator = ':', bool uppercase = true) where T : class
+    {
+        return rule.UseFactory(() =>
+        {
+            Span<byte> bytes = stackalloc byte[6];
+            Random.Shared.NextBytes(bytes);
+
+            if (separator is null)
+            {
+                var hex = Convert.ToHexString(bytes);
+                return uppercase ? hex : hex.ToLowerInvariant();
+            }
+
+            Span<char> chars = stackalloc char[17]; 
+            var pos = 0;
+            for (var i = 0; i < 6; i++)
+            {
+                if (i > 0) chars[pos++] = separator.Value;
+                bytes[i].TryFormat(chars[pos..], out var written, uppercase ? "X2" : "x2");
+                pos += written;
+            }
+            return new string(chars[..pos]);
+        });
     }
 
     private static string BuildRandomPrefix(LocaleData data)
